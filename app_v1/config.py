a29735f95abdf97b8e7a754f15422d75a31e4052
Istoshin Pavlo <pavloistoshin@gmail.com>
2025-10-24 15:13:21 +0300

    update

--- app_v1/config.py
+++ app_v1/config.py
@@ -1,13 +1,15 @@
-#from pydantic.env_settings import BaseSettings
+# /app_v1/config.py
+import os  # Импортируем модуль os
 
 
+# Теперь класс будет брать значения из переменных окружения Render
 class Settings():
-    smtp_email: str = "myrkry477@gmail.com"
-    smtp_password: str = "wfvp sqfi ugtz snay"
+    # Если переменная окружения не найдена, используем None
+    smtp_email: str = os.environ.get("SMTP_USERNAME")
+    smtp_password: str = os.environ.get("SMTP_PASSWORD")
 
-
+    # Это можно оставить, так как это не секрет
     test_email_to: str = "pashaistoshin90@gmail.com"
-
 
 
 settings = Settings()
