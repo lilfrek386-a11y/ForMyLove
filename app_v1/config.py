4cca24d76047f9befb39aee7e562151c73c9fb8b
Istoshin Pavlo <pavloistoshin@gmail.com>
2025-10-24 15:30:06 +0300

    update

--- app_v1/config.py
+++ app_v1/config.py
@@ -1,15 +1,4 @@
-# /app_v1/config.py
-import os  # Импортируем модуль os
-
-
-# Теперь класс будет брать значения из переменных окружения Render
 class Settings():
-    # Если переменная окружения не найдена, используем None
-    smtp_email: str = os.environ.get("SMTP_USERNAME")
-    smtp_password: str = os.environ.get("SMTP_PASSWORD")
-
-    # Это можно оставить, так как это не секрет
-    test_email_to: str = "pashaistoshin90@gmail.com"
-
-
+    smtp_email: str = "myrkry477@gmail.com"
+    smtp_password: str = "wfvp sqfi ugtz snay"
 settings = Settings()
